371c02444a4879936121ac5a02d1e0ba7a47d4de
Murat Koptur <mkoptur3@gmail.com>
2018-04-26 20:38:42 +0300

    Euler's Totient function added.

---
+++ euler_totient.hs
@@ -0,0 +1,7 @@
+module EulerTotient where
+
+coprimes :: Integral a => a -> [a]
+coprimes n = [x | x <- [1..(n-1)], gcd n x == 1]
+
+totient :: Integral a => a -> Int
+totient n = length $ coprimes n
