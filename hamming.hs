b07f0ef04bef44fdd0640f7e25e8f67565445071
Murat Koptur <mkoptur3@gmail.com>
2018-04-30 20:31:39 +0300

    Update Hamming.

--- hamming.hs
+++ hamming.hs
@@ -1,5 +1,6 @@
 module Hamming where
 
+hamming :: (Num a, Eq b) => [b] -> [b] -> a
 hamming str1 str2 = sum (map (\(x, y) -> check_eq x y) (zip str1 str2))
   where check_eq x y
           | x == y    = 0
