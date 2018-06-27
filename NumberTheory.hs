5c8c2b38b96b53683d4bfa28cc3a3405e69de658
Murat Koptur <mkoptur3@gmail.com>
2018-06-27 21:05:50 +0300

    Integer factor, isPrime, primes functions added

--- NumberTheory.hs
+++ NumberTheory.hs
@@ -12,8 +12,17 @@ myGCD a b
   | otherwise        = maximum [d | d <- [1..mab], isDivides d a, isDivides d b]
     where mab = min a b
 
+factors :: Integral a => a -> [a]
+factors n = [k | k <- [1..n], mod n k == 0]
+
 coprimes :: Integral a => a -> [a]
 coprimes n = [x | x <- [1..(n-1)], myGCD n x == 1]
 
 totient :: Integral a => a -> Int
 totient n = length $ coprimes n
+
+isPrime :: Integral a => a -> Bool
+isPrime n = n > 1 && []==[i | i <- [2..n-1], rem n i == 0]
+
+primes :: Integral a => a -> [a]
+primes n = [p | p <- [2..n], isPrime p]
