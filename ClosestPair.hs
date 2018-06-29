d8c9cae24f666f2a5563af6f595a0875bcbe7b9c
Murat Koptur <mkoptur3@gmail.com>
2018-06-29 22:38:02 +0300

    Better version

--- ClosestPair.hs
+++ ClosestPair.hs
@@ -8,6 +8,7 @@ data Point = Point Float Float
 instance Show Point where
   show (Point x y) = "(" ++ show x ++ ", " ++ show y ++ ")"
 
+-- Don't need Eq in this solution
 instance Eq Point where
   Point x1 y1 == Point x2 y2 = x1 == x2 && y1 == y2
 
@@ -16,7 +17,7 @@ distance (Point x1 y1) (Point x2 y2) = sqrt ((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 closestPair :: [Point] -> (Point, Point, Float)
 closestPair pointList = do
-  let distances = [(x, y, distance x y) | x <- pointList, y <- pointList, x /= y]
+  let distances = [(x, y, distance x y) | (x:ys) <- tails pointList, y <- ys]
   minimumBy (comparing (\(_,_,dist) -> dist)) distances
 
 main :: (Point, Point, Float)
