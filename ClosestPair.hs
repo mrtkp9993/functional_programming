cf8b2931e43f8bce019dda6dc3de7c16c475b840
Murat Koptur <mkoptur3@gmail.com>
2018-06-29 22:00:06 +0300

    ClosestPair.hs added

---
+++ ClosestPair.hs
@@ -0,0 +1,25 @@
+module ClosestPair where
+
+import Data.List
+import Data.Ord
+
+data Point = Point Float Float
+
+instance Show Point where
+  show (Point x y) = "(" ++ show x ++ ", " ++ show y ++ ")"
+
+instance Eq Point where
+  Point x1 y1 == Point x2 y2 = x1 == x2 && y1 == y2
+
+distance :: Point -> Point -> Float
+distance (Point x1 y1) (Point x2 y2) = sqrt ((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+closestPair :: [Point] -> (Point, Point, Float)
+closestPair pointList = do
+  let distances = [(x, y, distance x y) | x <- pointList, y <- pointList, x /= y]
+  minimumBy (comparing (\(_,_,dist) -> dist)) distances
+
+main :: (Point, Point, Float)
+main = do
+  let points = [Point 2 3, Point 12 30, Point 40 50, Point 5 1, Point 12 10, Point 3 4]
+  closestPair points
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 * Ackermann.hs: the Ackermann function, [Wiki](https://www.wikiwand.com/en/Ackermann_function).
 * CayleyTableUn.hs: Cayley table of ```U(n)```, [Cayley Table](https://www.wikiwand.com/en/Cayley_table), [Unitary Group](https://www.wikiwand.com/en/Unitary_group).
+* ClosestPair.hs: Closest pair of points problem [Wiki](https://www.wikiwand.com/en/Closest_pair_of_points_problem).
 * Hamming.hs: Hamming distance between two strings, [Wiki](https://www.wikiwand.com/en/Hamming_distance).
 * Quicksort.hs: Quicksort algorithm implementation, [Wiki](https://www.wikiwand.com/en/Quicksort).
 * NumberTheory.hs: Number-theoric functions.
